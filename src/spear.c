a957ef85ae597833725a8fa1434e91b756777319
Azon <syu.takayama@gmail.com>
2018-06-19 15:39:26 +0900

    solved spear.c

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,11 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  long long int K = 0;
+  for (int i = 0; i < n; i++) K += A[i] / m;
+  return K >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +17,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1145141919;
+  while (ub - lb > 1) {
+    int m = (lb + ub) / 2;
+    if (!p(m)) ub = m;
+    else lb = m;
+  }
+  printf("%d\n", lb);
 
   return 0;
 }
