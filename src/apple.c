5d9d4dbb103636c51f2b27b75550eab742cf0993
Azon <syu.takayama@gmail.com>
2018-06-19 15:39:18 +0900

    solved apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,7 +4,11 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int m){
+  long long int K = 0;
+  for (int i = 0; i < n; i++) K += A[i] / m + (A[i] % m != 0);
+  return K <= k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
@@ -12,6 +16,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1145141919;
+  while (ub - lb > 1) {
+    int m = (lb + ub) / 2;
+    if (p(m)) ub = m;
+    else lb = m;
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
