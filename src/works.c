ed8197ef806934a80e7d738cdb3144a97b465a87
Azon <syu.takayama@gmail.com>
2018-06-19 15:39:35 +0900

    solved works.c

--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int K = 1;
+  int w = 0;
+  for (int i = 0; i < n; i++) {
+    if (A[i] > m) return 0;
+    w += A[i];
+    if (w > m) {
+      K++;
+      w = A[i];
+    }
+  }
+  return K <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +25,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1145141919;
+  while (ub - lb > 1) {
+    int m = (lb + ub) / 2;
+    if (p(m)) ub = m;
+    else lb = m;
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
